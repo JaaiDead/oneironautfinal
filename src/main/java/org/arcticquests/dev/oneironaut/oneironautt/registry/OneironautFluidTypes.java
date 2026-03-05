56f7809872e1ff8740dd53828f48478767306433
Jaai <arcticquests.dev@gmail.com>
2026-03-05 22:28:43 +0530

    Final VersioN!!!

--- src/main/java/org/arcticquests/dev/oneironaut/oneironautt/registry/OneironautFluidTypes.java
+++ src/main/java/org/arcticquests/dev/oneironaut/oneironautt/registry/OneironautFluidTypes.java
@@ -1,6 +1,7 @@
 package org.arcticquests.dev.oneironaut.oneironautt.registry;
 
 import com.mojang.blaze3d.shaders.FogShape;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.FogRenderer;
@@ -8,6 +9,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fluids.FluidStack;
@@ -45,17 +47,22 @@ public class OneironautFluidTypes {
 
                         @Override
                         public int getTintColor() {
-                            return 0xFFC87FFA;
+                            return 0xFFAD73CE;
                         }
 
                         @Override
                         public void modifyFogRender(Camera camera, FogRenderer.FogMode mode, float renderDistance, float partialTick, float nearDistance, float farDistance, FogShape shape) {
-                            IClientFluidTypeExtensions.super.modifyFogRender(camera, mode, renderDistance, partialTick, nearDistance, farDistance, shape);
+                            float fogStart = 10.0F;
+                            float fogEnd = 12.0F;
+                            RenderSystem.setShaderFogStart(fogStart);
+                            RenderSystem.setShaderFogEnd(fogEnd);
                         }
+
+                        public static final Vector3f fogColor = new Vector3f(0.678F, 0.451F, 0.808F);
 
                         @Override
                         public @NotNull Vector3f modifyFogColor(Camera camera, float partialTick, ClientLevel level, int renderDistance, float darkenWorldAmount, Vector3f fluidFogColor) {
-                            return new Vector3f(0.85f, 0.7f, 0.97f);
+                            return fogColor;
                         }
                     });
                 }
